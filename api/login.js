7f131ba3be24a6d8a63da7788760206a47771069
Vinzzyy <98145666+Vinzzyyyyyy@users.noreply.github.com>
2025-12-10 20:32:43 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -106,7 +106,7 @@ export default async function handler(req, res) {
         const token = jwt.sign(
           { id: user._id, email: user.email },
           JWT_SECRET,
-          { expiresIn: "1m" }
+          { expiresIn: "30m" }
         );
 
         return res.status(200).json({ message: "Login berhasil", token });
